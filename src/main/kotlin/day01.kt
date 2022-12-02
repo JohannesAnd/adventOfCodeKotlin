7705a85035ca7ab512d51903f54408969dc5c519
Johannes <johanes1@online.no>
2022-12-02 09:10:44 +0100

    day1

---
+++ src/main/kotlin/day01.kt
@@ -0,0 +1,40 @@
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+private fun getInventories(): List<Int> {
+    val inventories = mutableListOf(0)
+
+    File("src/main/resources/day01.txt").bufferedReader().forEachLine { line ->
+        run {
+            if (line.isEmpty()) {
+                inventories.add(0)
+            } else {
+                inventories[inventories.lastIndex] += line.toInt()
+            }
+        }
+    }
+
+    return inventories
+}
+
+fun part1(): Int {
+    return getInventories().maxOrNull() ?: 0
+}
+
+fun part2(): Int {
+    return getInventories().sorted().takeLast(3).sum()
+}
+
+fun main() {
+    val part1Answer: Int
+    val part1Time = measureTimeMillis { part1Answer = part1() }
+    println("Part one:")
+    println("Answer: $part1Answer")
+    println("Time: $part1Time")
+
+    val part2Answer: Int
+    val part2Time = measureTimeMillis { part2Answer = part2() }
+    println("Part two:")
+    println("Answer: $part2Answer")
+    println("Time: $part2Time")
+}
